843c0eb48a69b86ce09f4fe3b33aec60b002acbf
Ben Harrington <harringtonben@icloud.com>
2018-01-25 20:07:01 -0600

    have department base class finished

---
+++ architecture/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace architecture
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public string Supervisor { get; set; }
+        public int Employee_Count { get; set; }
+
+        public void Deparment_Description()
+        {
+            Console.WriteLine($"{Name} is overseen by {Supervisor} and has {Employee_Count} number of employees.");
+        }
+    }
+}
