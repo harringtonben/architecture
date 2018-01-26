c069b0b480975ce34fd2aaf040e4d1ced2616bfc
Ben Harrington <harringtonben@icloud.com>
2018-01-25 20:24:11 -0600

    finished IT class

---
+++ architecture/Departments/IT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace architecture.Departments
+{
+    class IT : Department
+    {
+        public string TechStack { get; set; }
+        public bool AreLazy { get; set; }
+
+        public void OurDevelopers()
+        {
+            if (AreLazy)
+            {
+                Console.WriteLine($"Our developers all code in {TechStack} but they are lazy as hell.");
+            }
+            else
+            {
+                Console.WriteLine($"Our developers all code in {TechStack} and they rock the cazba!");
+            }
+        }
+    }
+}
