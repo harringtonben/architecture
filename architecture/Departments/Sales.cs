ae055f61b6f6d40ad7368d6328ef964267a8fc02
Ben Harrington <harringtonben@icloud.com>
2018-01-25 20:29:48 -0600

    finished my last class to add

---
+++ architecture/Departments/Sales.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace architecture.Departments
+{
+    class Sales
+    {
+        public int NumberOfDeals { get; set; }
+        public bool AreTools { get; set; }
+
+        public void SalesGoals()
+        {
+            if (AreTools)
+            {
+                Console.WriteLine($"Our sales guys have closed {NumberOfDeals} this year, but man are they tools.");
+            }
+            else
+            {
+                Console.WriteLine($"Our sales guys have closed {NumberOfDeals} this year, and they are actually pretty alright.");
+            }
+
+        }
+    }
+}
